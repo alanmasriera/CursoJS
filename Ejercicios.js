f75f3b76696e6db656c7f87669fa6a86a8a69390
Alan Masriera PC <alan.masriera@gmail.com>
2021-06-11 15:31:06 -0300

    Ejercicio 24 resuelto

--- Ejercicios.js
+++ Ejercicios.js
@@ -1,2 +1,12 @@
 //24) Programa una función que dado un arreglo de números devuelva un objeto con dos arreglos, el primero tendrá los numeros ordenados en forma ascendente 
 //y el segundo de forma descendiente, pe. miFuncion([7, 5,7,8,6]) devolverá { asc: [5,6,7,7,8], desc: [8,7,7,6,5] }.
+const ordenarArreglo = (arr = undefined) => {
+    return console.info({
+        arr,
+        asc: arr.map(el => el).sort(),
+        desc: arr.map(el => el).sort().reverse()
+    })
+}
+
+ordenarArreglo([1,2,3,4,8,5,4])
+ 
