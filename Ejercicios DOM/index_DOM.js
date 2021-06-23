a7bbc70f2aa1591c32daac49c4d118983aa63786
alanmasriera <alan.masriera@gmail.com>
2021-06-23 14:42:41 -0300

    dark/light theme terminado, con local storage

--- Ejercicios DOM/index_DOM.js
+++ Ejercicios DOM/index_DOM.js
@@ -1,7 +1,9 @@
+import scrollTopButton from "./js/boton_scroll.js";
 import countdown from "./js/cuenta_regresiva.js";
 import hamburgerMenu from "./js/menu_hamburguesa.js";
 import { showTime, reloj, alarma} from "./js/reloj.js";
 import { shortcuts, moveBall } from "./js/teclado.js";
+import darkTheme from "./js/tema_oscuro.js";
 
 const  d = document
 console.log("En index_DOM")
@@ -10,9 +12,13 @@ d.addEventListener("DOMContentLoaded", (e) => {
     reloj("#iniciar-reloj","#parar-reloj")
     alarma("assets/alarma-morning-mix.mp3","#iniciar-alarma","#parar-alarma")
     countdown("countdown","Jun 01 2022 00:00:00","Feliz nuevo año")
+    scrollTopButton(".scroll-top-btn")
+    
 })
 
 d.addEventListener("keydown",e=>{
     shortcuts(e)
     moveBall(e,".ball",".stage")
 })
+
+darkTheme(".dark-theme-btn","dark-mode")
---
+++ Ejercicios DOM/js/tema_oscuro.js
@@ -0,0 +1,36 @@
+const d = document
+const ls = localStorage
+export default function darkTheme(btn, classDark){
+    const $themeBtn = d.querySelector(btn)
+    const $selectors = d.querySelectorAll("[data-dark]")
+    console.log($selectors)
+    let moon = "🌙"
+    let sun = "☀️"
+    const lightMode = () => {
+        $selectors.forEach(el => el.classList.add(classDark))
+        $themeBtn.textContent = sun
+        ls.setItem("theme","light")
+    }
+    const darkMode = () => {
+        $selectors.forEach(el => el.classList.remove(classDark))
+        $themeBtn.textContent = moon
+        ls.setItem("theme","dark")
+    }
+
+    d.addEventListener("click", e=>{
+        if(e.target.matches(btn)){
+            if($themeBtn.textContent === moon){
+                lightMode()
+            }
+            else{
+                darkMode()
+            }
+        }
+    })
+
+    d.addEventListener("DOMContentLoaded", e =>{
+        if(ls.getItem("theme") === null) ls.setItem("theme","light")
+        if(ls.getItem("theme") === "light") lightMode()
+        else darkMode()
+    })
+}
