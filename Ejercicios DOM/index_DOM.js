75859837e4e3fc458d89b04834c2538aad2a21af
alanmasriera <alan.masriera@gmail.com>
2021-06-24 13:16:07 -0300

    Ejercicio Responsive tester terminado

--- Ejercicios DOM/index_DOM.js
+++ Ejercicios DOM/index_DOM.js
@@ -2,6 +2,7 @@ import scrollTopButton from "./js/boton_scroll.js";
 import countdown from "./js/cuenta_regresiva.js";
 import hamburgerMenu from "./js/menu_hamburguesa.js";
 import responsiveMedia from "./js/objeto_responsive.js";
+import responsiveTester from "./js/prueba_responsive.js";
 import { showTime, reloj, alarma} from "./js/reloj.js";
 import { shortcuts, moveBall } from "./js/teclado.js";
 import darkTheme from "./js/tema_oscuro.js";
@@ -26,6 +27,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
     `<a href="https://goo.gl/maps/tMARdKVeEzoPeMab6" target="_blank" rel="noopener"> Ver mapa </a> `,
     `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27243.451159088214!2d-64.20699175!3d-31.402238399999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x943298830933ad09%3A0xb72fd713f4c77c4e!2sNuevocentro%20Shopping!5e0!3m2!1ses-419!2sar!4v1624543309764!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>`
     )
+    responsiveTester("responsive-tester")
 })
 
 d.addEventListener("keydown",e=>{
