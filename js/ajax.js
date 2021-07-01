0e2050149f81481d602a4e23ceef976797eace74
Alan Masriera PC <alan.masriera@gmail.com>
2021-07-01 00:44:05 -0300

    Ajax fetchAsync

--- js/ajax.js
+++ js/ajax.js
@@ -52,4 +52,31 @@
         .finally(()=>{
             console.log("Este mensaje se ejecutara independientemente de la respuesta")
         })
- })()
+ })();
+
+ (() => {
+    const $fetchAsync = document.getElementById("fetch-async")
+    const $fragment = document.createDocumentFragment()
+    
+    async function getData(){
+        try{
+            let res = await fetch("https://jsonplaceholder.typicode.com/users")
+            let json = await res.json()
+            //if(!res.ok){throw new Error("Mensaje")}
+            if(!res.ok){throw {status: res.status, statusText: res.statusText}}
+            json.forEach(el =>{
+                const $li = document.createElement("li")
+                $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`
+                $fragment.appendChild($li)
+            })
+            $fetchAsync.appendChild($fragment)
+        }catch(err){
+            let message = err.statusText || "Ocurrio un error"
+            $fetchAsync.innerHTML = `Error ${err.status}: ${message}`
+        }finally{
+
+        }
+    }
+    getData()
+    
+})()
